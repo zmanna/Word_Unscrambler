a56f2b549e76d2cac05ba9e2fb41e21efc0543f2
Willem Battey <115966859+willbtty@users.noreply.github.com>
2024-11-10 17:26:44 -0600

    First push of C#

---
+++ RustGameAPI/Models/FriendDTO.cs
@@ -0,0 +1,9 @@
+﻿// Models/FriendDTO.cs
+namespace RustGameAPI.Models
+{
+    public class FriendDTO
+    {
+        public int UserID { get; set; }
+        public int FriendUserID { get; set; }
+    }
+}
---
+++ RustGameAPI/Models/User.cs
@@ -0,0 +1,23 @@
+﻿// Models/User.cs
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace RustGameAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [BindNever]
+        public int UserID { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        // Navigation properties
+        public ICollection<Friend> Friends { get; set; } = new List<Friend>();
+        public ICollection<Friend> FriendOf { get; set; } = new List<Friend>();
+    }
+}
